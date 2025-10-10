85a104e520378fd193838951f0386282a67df698
MaheenSohail <maheensohailsohailanwar@gmail.com>
2025-10-10 19:18:17 +0500

    Add files via upload

---
+++ js-assignment all/Js-Assignment-7/file.js
@@ -0,0 +1,517 @@
+var products = [
+    {
+    title: "Umrah Mubarak",
+    description: "Elegant fondant cake in white and gold, featuring Kaaba details — a sweet way to celebrate a blessed journey.",
+    image:"./cake.jpeg"
+    },
+        {
+    title: "Hajj Mubarak",
+    description: "Elegant fondant cake in white, black, and gold — celebrating a sacred journey with sweetness and grace.",
+    image:"./cake1.jpeg"
+    },
+        {
+    title: "Umrah Mubarak",
+    description: "Elegant fondant cake in white and gold, featuring Kaaba details — a sweet way to celebrate a blessed journey.",
+    image:"./cake2.jpeg"
+    },
+        {
+    title: "Umrah Mubarak",
+    description: "Elegant fondant cake in white and gold, featuring Kaaba details — a sweet way to celebrate a blessed journey.",
+    image:"./cake6.jpg"
+    },
+        {
+    title: "Engagement",
+    description: "Engagement Mubarak! A classy fondant cake in soft tones — celebrating love, joy, and new beginnings. 💍",
+    image:"./cake3.jpeg"
+    },
+        {
+    title: "Nikkah Mubarak",
+    description: "Celebrate this blessed union with a classy fondant cake — Nikkah Mubarak! 💫",
+    image:"./cake4.jpeg"
+    },
+        {
+    title: "Mayun",
+    description: "A vibrant fondant cake inspired by Mayun celebrations — sweet, traditional, and full of mehndi vibes! 🌼",
+    image:"./cake5.jpeg"
+    },
+        {
+    title: "Wedding Mubarak",
+    description: "Celebrate this beautiful beginning with a classy fondant cake — Wedding Mubarak! 🤍",
+    image:"./cake7.jpg"
+    },
+         {
+    title: "Boss Baby",
+    description: "A playful fondant cake inspired by Boss Baby — perfect for your little one’s special day! 🎉",
+    image:"./cake8.jpeg"
+    },
+         {
+    title: "Boss Baby",
+    description: "A playful fondant cake inspired by Boss Baby — perfect for your little one’s special day! 🎉",
+    image:"./cake9.jpeg"
+    },
+          {
+    title: "Boss Baby",
+    description: "A playful fondant cake inspired by Boss Baby — perfect for your little one’s special day! 🎉",
+    image:"./cake10.jpg"
+    },
+            {
+    title: "Boss Baby",
+    description: "A playful fondant cake inspired by Boss Baby — perfect for your little one’s special day! 🎉",
+    image:"./cake11.jpg"
+    },
+            {
+    title: "Car",
+    description: "A cool car-themed fondant cake — perfect for your little racer’s big day! 🏁🎉",
+    image:"./cake12.jpeg"
+    },        {
+    title: "Car",
+    description: "Vroom Vroom! A cool car-themed fondant cake for your little racer! 🏎️💨",
+    image:"./cake13.jpeg"
+    },
+            {
+    title: "Car",
+    description: "Vroom Vroom! A stylish fondant cake for car lovers — full of speed, fun, and sweetness! 🚗✨",
+    image:"./cake14.jpeg"
+    },
+            {
+    title: "Car",
+    description: "Vroom Vroom! A stylish fondant cake for car lovers — full of speed, fun, and sweetness! 🚗✨",
+    image:"./cake15.jpeg"
+    },
+            {
+    title: "Car",
+    description: "Vroom Vroom! A stylish fondant cake for car lovers — full of speed, fun, and sweetness! 🚗✨",
+    image:"./cake16.jpeg"
+    },
+            {
+    title: "Cocomelon",
+    description: "Happy Birthday! A cute and colorful Cocomelon fondant cake — full of fun, smiles, and sweetness! 🎉💚",
+    image:"./cake17.jpeg"
+    },
+            {
+    title: "Cocomelon",
+    description: "Happy Birthday! A cute and colorful Cocomelon fondant cake — full of fun, smiles, and sweetness! 🎉💚",
+    image:"./cake18.jpeg"
+    },
+            {
+    title: "Cocomelon",
+    description: "A bright and playful Cocomelon themed cake — perfect for your little one’s special day! 🍉🎂",
+    image:"./cake19.jpeg"
+    },
+            {
+    title: "Cocomelon",
+    description: "A bright and playful Cocomelon themed cake — perfect for your little one’s special day! 🍉🎂",
+    image:"./cake20.jpeg"
+    },
+            {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake21.jpeg"
+    },
+            {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake22.jpeg"
+    },
+            {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake23.jpeg"
+    },
+            {
+    title: "Fish",
+    description: "Dive into sweetness! A colorful Fish Theme fondant cake full of ocean fun and smiles. 🐟",
+    image:"./cake24.jpeg"
+    },
+            {
+    title: "Fish",
+    description: "Dive into sweetness! A colorful Fish Theme fondant cake full of ocean fun and smiles. 🐟",
+    image:"./cake25.jpeg"
+    },
+            {
+    title: "Fish",
+    description: "A cute underwater-inspired fondant cake — Fish Theme fun for your little one’s big day! 🐠",
+    image:"./cake26.jpeg"
+    },
+            {
+    title: "Fish",
+    description: "A cute underwater-inspired fondant cake — Fish Theme fun for your little one’s big day! 🐠",
+    image:"./cake27.jpeg"
+    },
+                {
+    title: "Boss Baby",
+    description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+    image:"./cake28.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈",
+    image:"./cake29.jpg"
+    },
+                {
+    title: "Mayun",
+    description: "Mayun Mubarak! A bright yellow fondant cake full of joy, love, and festive vibes. 🌿",
+    image:"./cake30.jpg"
+    },
+                {
+    title: "Engagement",
+    description: "Engagement Mubarak! A classy fondant cake in soft tones — celebrating love, joy, and new beginnings. 💍",
+     image:"./cake31.jpg"
+    },
+                {
+    title: "Apple",
+    description: "Celebrate in style with a classy Apple iPhone theme cake — smart, sleek, and sweet! 💻📱",
+    image:"./cake32.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake33.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake34.jpg"
+    },
+                {
+    title: "Boss Baby",
+    description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+    image:"./cake35.jpg"
+    },
+                {
+    title: "Fish",
+    description: "A cute underwater-inspired fondant cake — Fish Theme fun for your little one’s big day! 🐠",
+    image:"./cake36.jpg"
+    },
+     {
+       title: "Boss Baby",
+       description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+       image:"./cake37.jpg"
+    },
+     {
+       title: "Boss Baby",
+       description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+      image:"./cake38.jpg"
+    },
+     {
+       title: "Boss Baby",
+       description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+       image:"./cake39.jpg"
+    },
+     {
+       title: "Boss Baby",
+       description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+       image:"./cake40.jpg"
+    },
+     {
+          title: "Boss Baby",
+       description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+      image:"./cake41.jpg"
+    },
+     {
+        title: "Boss Baby",
+       description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+      image:"./cake42.jpg"
+    },
+     {
+       title: "Boss Baby",
+       description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+       image:"./cake43.jpg"
+    },
+     {
+        title: "Boss Baby",
+       description: "Celebrating the tiniest boss in town with a fun fondant cake — Boss Baby theme! 👔🍼",
+      image:"./cake44.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake45.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake46.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake47.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake48.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake49.jpg"
+    },
+                {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake50.jpg"
+    },
+      {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake51.jpg"
+    },
+      {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake52.jpg"
+    },
+      {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake53.jpg"
+    },
+      {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake54.jpg"
+    },
+      {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake55.jpg"
+    },
+      {
+    title: "Cocomelon",
+    description: "Celebrate with joy and color — Cocomelon fondant cake made with love for your tiny star! 🌈💛",
+    image:"./cake56.jpg"
+    },
+      {
+    title: "Dinosaur",
+    description: "A roaringly adorable Dinosaur Cake — perfect for your little dino lover’s big day! 🎉🦕",
+    image:"./cake57.jpeg"
+    },
+      {
+    title: "Dinosaur",
+    description: "A roaringly adorable Dinosaur Cake — perfect for your little dino lover’s big day! 🎉🦕",
+    image:"./cake58.jpeg"
+    },
+      {
+    title: "Dinosaur",
+    description: "A roaringly adorable Dinosaur Cake — perfect for your little dino lover’s big day! 🎉🦕",
+    image:"./cake59.jpeg"
+    },
+      {
+    title: "Dinosaur",
+    description: "A roaringly adorable Dinosaur Cake — perfect for your little dino lover’s big day! 🎉🦕",
+    image:"./cake60.jpeg"
+    },
+     {
+    title: "Dinosaur",
+    description: "A roaringly adorable Dinosaur Cake — perfect for your little dino lover’s big day! 🎉🦕",
+    image:"./cake61.jpeg"
+    },
+     {
+    title: "Dinosaur",
+    description: "A roaringly adorable Dinosaur Cake — perfect for your little dino lover’s big day! 🎉🦕",
+    image:"./cake62.jpeg"
+    },
+      {
+    title: "Jungle Theme",
+    description: "Welcome to the jungle! A fun fondant cake full of cute animals, greenery, and adventure! 🦁🍃",
+    image:"./cake63.jpeg"
+    },
+      {
+    title: "Jungle Theme",
+    description: "A wild and colorful Jungle Theme fondant cake — perfect for your little explorer’s big day! 🐘🌴",
+    image:"./cake64.jpeg"
+    }, 
+     {
+    title: "Jungle Theme",
+    description: "Welcome to the jungle! A fun fondant cake full of cute animals, greenery, and adventure! 🦁🍃",
+    image:"./cake65.jpeg"
+    },
+    {
+    title: "Jungle Theme",
+    description: "Welcome to the jungle! A fun fondant cake full of cute animals, greenery, and adventure! 🦁🍃",
+    image:"./cake66.jpeg"
+    },
+      {
+    title: "Jungel Theme",
+    description: "A wild and colorful Jungle Theme fondant cake — perfect for your little explorer’s big day! 🐘🌴",
+    image:"./cake67.jpeg"
+    },
+      {
+    title: "Jungle Theme",
+    description: "Welcome to the jungle! A fun fondant cake full of cute animals, greenery, and adventure! 🦁🍃",
+    image:"./cake68.jpeg"
+    },  {
+    title: "Jungle Theme",
+    description: "Welcome to the jungle! A fun fondant cake full of cute animals, greenery, and adventure! 🦁🍃",
+    image:"./cake69.jpeg"
+    },
+         {
+    title: "Jungle Theme",
+    description: "Welcome to the jungle! A fun fondant cake full of cute animals, greenery, and adventure! 🦁🍃",
+    image:"./cake70.jpeg"
+    },
+      {
+    title: "Doll",
+    description: "A dreamy Blue Doll Theme fondant cake — perfect for your little princess! 💖🎂",
+    image:"./cake71.jpeg"
+    }, 
+     {
+    title: "Doll",
+    description: "A dreamy Baby Doll fondant cake — perfect for your little angel’s special day! 🎂🌸",
+    image:"./cake72.jpeg"
+    },
+         {
+    title: "Doll",
+    description: "Barbie vibes only! A glamorous fondant cake in pink and sparkle — made for your little fashion queen! 👑💗",
+    image:"./cake73.jpeg"
+    },
+      {
+    title: "Doll",
+    description: "A dreamy Baby Doll fondant cake — perfect for your little angel’s special day! 🎂🌸",
+    image:"./cake74.jpeg"
+    }, 
+     {
+    title: "Doll",
+    description: "A dreamy Baby Doll fondant cake — perfect for your little angel’s special day! 🎂🌸",
+    image:"./cake75.jpeg"
+    },   
+      {
+    title: "Doll",
+    description: "Frozen magic! A dreamy fondant cake featuring Elsa, Anna & Olaf — full of color, sparkle, and winter fun! ❄️💙",
+    image:"./cake76.jpeg"
+    },
+      {
+    title: "Doll",
+    description: "Frozen magic! A dreamy fondant cake featuring Elsa, Anna & Olaf — full of color, sparkle, and winter fun! ❄️💙",
+    image:"./cake77.jpeg"
+    },  {
+    title: "Doll",
+    description: "A dreamy Baby Doll fondant cake — perfect for your little angel’s special day! 🎂🌸",
+    image:"./cake78.jpeg"
+    },
+         {
+    title: "Doll",
+    description: "Frozen magic! A dreamy fondant cake featuring Elsa, Anna & Olaf — full of color, sparkle, and winter fun! ❄️💙",
+    image:"./cake79.jpeg"
+    },
+      {
+    title: "Doll",
+    description: "Sweet, stylish, and sparkling — a beautiful Doll Cake for a magical celebration!",
+    image:"./cake80.jpeg"
+    },
+    {
+    title: "Doll",
+    description: "Sweet, stylish, and sparkling — a beautiful Doll Cake for a magical celebration!",
+    image:"./cake81.jpeg"
+    },
+    {
+    title: "Doll",
+    description: "Frozen magic! A dreamy fondant cake featuring Elsa, Anna & Olaf — full of color, sparkle, and winter fun! ❄️💙",
+    image:"./cake82.jpeg"
+    },
+    {
+    title: "Doll",
+    description: "A dreamy Pink Doll Theme fondant cake — perfect for your little princess! 💖🎂",
+    image:"./cake83.jpeg"
+    },{
+    title: "Doll",
+    description: "Frozen magic! A dreamy fondant cake featuring Elsa, Anna & Olaf — full of color, sparkle, and winter fun! ❄️💙",
+    image:"./cake84.jpeg"
+    },
+    {
+    title: "Doll",
+    description: "Sweet, stylish, and sparkling — a beautiful Doll Cake for a magical celebration!",
+    image:"./cake85.jpeg"
+    },
+    {
+    title: "Doll",
+    description: "Barbie vibes only! A glamorous fondant cake in pink and sparkle — made for your little fashion queen! 👑💗",
+    image:"./cake86.jpeg"
+    },
+    {
+    title: "Butterfly",
+    description: "Elegant butterfly fondant cake with delicate wings and soft pastel tones.",
+    image:"./cake87.jpeg"
+    },
+    {
+    title: "Butterfly",
+    description: "Beautifully crafted butterfly fondant cake, full of charm and sweetness.",
+    image:"./cake88.jpeg"
+    },
+        {
+    title: "Makeup",
+    description: "Makeup theme cake with lipsticks, brushes, and sparkle!",
+    image:"./cake89.jpeg"
+    },
+    {
+    title: "Makeup",
+    description: "Makeup lover’s dream cake — elegant, stylish, and delicious!",
+    image:"./cake90.jpeg"
+    },
+        {
+    title: "Masha and the Bear",
+    description: "Cute MashaBear theme cake for little ones’ special day!🐻",
+    image:"./cake91.jpeg"
+    },
+    {
+    title: "Masha and the Bear",
+    description: "Masha and the Bear fondant cake — sweet, playful, and delightful!🐻🎀",
+    image:"./cake92.jpeg"
+    },    {
+    title: "Mickey Mouse",
+    description: "Mickey Mouse cake — bringing Disney magic to your celebration!🎈🐭",
+    image:"./cake93.jpeg"
+    },
+    {
+    title: "Mickey Mouse",
+    description: "Celebrate with everyone’s favorite character — Mickey Mouse!🐭",
+    image:"./cake94.jpeg"
+    },    {
+    title: "Mickey Mouse",
+    description: "Classic Mickey Mouse fondant cake — fun, bright, and joyful!🎈",
+    image:"./cake95.jpeg"
+    },
+
+]
+
+const container = document.getElementById("container");
+
+const productCard = (cakeProduct) => {
+  return `
+ <div class="card" style="width: 18rem;">
+      <img src="${cakeProduct.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${cakeProduct.title}</h5>
+        <p class="card-text">${cakeProduct.description}</p>
+       <a href="https://www.facebook.com/Fondantcake.pk/?ref=page_internal&mt_nav=0&paipv=1&_rdr" target="_blank" class="btn btn-primary">Order Now</a>
+      </div>
+    </div>
+`
+}
+
+var product = products.map(productCard).join("");
+container.innerHTML = product;
+
+const productSearch = (products, searchItem) => {
+
+  let filterProducts = products.filter((cakeProduct) => {
+    return cakeProduct.title.toLowerCase().includes(searchItem.toLowerCase())
+  });
+
+  return filterProducts;
+
+}
+
+const searchInput = (searchItem) => {
+
+  let searchResult = productSearch(products, searchItem);
+  if (searchResult.length > 0) {
+     product = searchResult.map(productCard).join("")
+    container.innerHTML = product;
+  } else {
+    container.innerHTML = `<h1>No Match Found!</ h1>`
+
+  }
+}
